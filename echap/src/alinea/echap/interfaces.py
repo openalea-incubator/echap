e38cfeaf48ab5a1e81155ddfc92ed9cb8f23f2e3
Romain Chapuis <chapuis.r@hotmail.com>
2013-03-20 16:17:05 +0000

    Add penetrated_doses and surfacic_doses
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@2450 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/src/alinea/echap/interfaces.py
+++ echap/src/alinea/echap/interfaces.py
@@ -1,11 +1,9 @@
 """defines interfaces between g and the different models of echap project
 """
+from alinea.pearl.pearl_leaf import *
 
 
-
-def pesticide_surfacic_decay(g, 
-             decay_model,
-             label="LeafElement"):
+def pesticide_surfacic_decay(g, decay_model, label="LeafElement"):
     """ Interface between g and the decay model of Pearl
 
     Parameters:
@@ -14,11 +12,18 @@ def pesticide_surfacic_decay(g,
 
     doses is a dict compound_name:ammount
     """
-    doses = g.property('surfacic_doses') 
-    for vid, d in doses.iteritems():
+    surfacic_doses = g.property('surfacic_doses') 
+    penetrated_doses = g.property('penetrated_doses') 
+    for vid, d in surfacic_doses.iteritems():
         if g.label(vid).startswith(label):
             for compound_name,compound_dose in d.iteritems():
                 leaf = g.node(vid)
                 microclimate = {'Tair':leaf.temp}
-                doses[vid][compound_name] = decay_model.decay(compound_name,compound_dose,microclimate)
+                surfacic_doses[vid][compound_name] = decay_model.decay(compound_name,compound_dose,microclimate)[0]
+    for vid, d in penetrated_doses.iteritems():
+        if g.label(vid).startswith(label):
+            for compound_name,compound_dose in d.iteritems():
+                leaf = g.node(vid)
+                microclimate = {'Tair':leaf.temp}
+                penetrated_doses[vid][compound_name] = decay_model.decay(compound_name,compound_dose,microclimate)[3]
     return g
