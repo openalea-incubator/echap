5eb9f79195c41221b3545a74bee63b86e31c40fd
Romain Chapuis <chapuis.r@hotmail.com>
2013-03-22 12:45:42 +0000

    Add echap_wralea Macros _wralea_ file
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@2468 0978ab36-7443-0410-8248-c2aa344a765a

---
+++ echap/src/alinea/echap_wralea/Macros/__wralea__.py
@@ -0,0 +1,24 @@
+
+# This file has been generated at Fri Mar 22 13:50:27 2013
+
+from openalea.core import *
+
+
+__name__ = 'Alinea.Echap.Macros'
+
+__editable__ = True
+__description__ = ''
+__license__ = 'CeCILL-C'
+__url__ = 'http://openalea.gforge.inria.fr'
+__alias__ = []
+__version__ = '0.8.0'
+__authors__ = ''
+__institutes__ = ''
+__icon__ = ''
+
+
+__all__ = []
+
+
+
+
