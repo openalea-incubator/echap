5e5c193a450aba2df5955b6d60741f702ab90733
Romain Chapuis <chapuis.r@hotmail.com>
2013-03-22 14:22:14 +0000

    Add Annua_loop file
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@2471 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/src/alinea/echap_wralea/Annual_loop/__wralea__.py
+++ echap/src/alinea/echap_wralea/Annual_loop/__wralea__.py
@@ -1,5 +1,5 @@
 
-# This file has been generated at Fri Mar 22 15:19:25 2013
+# This file has been generated at Fri Mar 22 15:27:18 2013
 
 from openalea.core import *
 
@@ -7,12 +7,14 @@ from openalea.core import *
 __name__ = 'annual_loop'
 
 __editable__ = True
-__version__ = ''
 __description__ = ''
 __license__ = ''
-__authors__ = ''
 __url__ = ''
+__alias__ = []
+__version__ = ''
+__authors__ = ''
 __institutes__ = ''
+__icon__ = ''
 
 
 __all__ = []
