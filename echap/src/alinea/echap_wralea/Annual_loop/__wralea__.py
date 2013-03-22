56a6330d2bf091c88c0150ff54990d8745db3ad2
Romain Chapuis <chapuis.r@hotmail.com>
2013-03-22 15:20:29 +0000

    Create Annual_loop file
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@2475 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/src/alinea/echap_wralea/Annual_loop/__wralea__.py
+++ echap/src/alinea/echap_wralea/Annual_loop/__wralea__.py
@@ -1,24 +1,19 @@
-
-# This file has been generated at Fri Mar 22 15:27:18 2013
+# This file has been generated at Fri Mar 22 15:17:19 2013
 
 from openalea.core import *
 
 
-__name__ = 'annual_loop'
+__name__ = 'Alinea.Echap.Annual_loop'
 
 __editable__ = True
 __description__ = ''
-__license__ = ''
-__url__ = ''
+__license__ = 'CeCILL-C'
+__url__ = 'http://openalea.gforge.inria.fr'
 __alias__ = []
-__version__ = ''
+__version__ = '0.8.0'
 __authors__ = ''
 __institutes__ = ''
 __icon__ = ''
 
 
 __all__ = []
-
-
-
-
