e8b97d913943edefc1457c8f56e45c725322337d
Romain Chapuis <chapuis.r@hotmail.com>
2013-04-11 15:15:46 +0000

    add procedure for caribu light interception
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@2595 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/example/CaribuUsage.py
+++ echap/example/CaribuUsage.py
@@ -87,16 +87,18 @@ scene = plot3d(g) # on produit la scene
 
 
 # source emission = lumiere intensite 1, oriente selon vecteur (0,0 -1)
-source = (1,(0,0,-1))
+# Import alinea.caribu.sky_tools.turtle as turtle
+# Pour rayonnement diffu: source = turtle.
+#energy, emission, direction, elevation, azimuth = turtle.turtle(sectors='46', energy=1) sectors='16'
+# Tout est fait avec 1 en energi de départ
+#sources = zip(energy, direction)
+source = (1,(0,0,-1))  
 c_scene = CaribuScene()    
 idmap = c_scene.add_Shapes(scene)    
 c_scene.addSources(source)
 output = c_scene.runCaribu(infinity=False)
 resultat = c_scene.output_by_id(output, idmap)['Einc']
+# output = Incident_parameter = einc / Area (pour intput pearl)(tester si Area nul ou pas) Einc / Area = EiInf + EiSup (pour vérifier meme si Area nuls)
+# pour la pluie -> einc, et pour un rayonnement -> einc / area.
+#résultat à mettre sur g en écrasant l'ancienne valeur (dans local_meteo)
 
-
-# Je vien de voir que pour faire un plot de valeur 'pechees' sur g, tu pouvais jeter un oeil sur alep/test/tesprotocoladel, lignes 540 et plus
-
-# a bientot !
-
-#christian
