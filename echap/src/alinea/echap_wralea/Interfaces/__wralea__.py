70f3010a65a241167ff5a9e6418f2c648535b779
Romain Chapuis <chapuis.r@hotmail.com>
2013-04-16 15:53:20 +0000

    Update annual loop - interfaces
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@2611 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/src/alinea/echap_wralea/Interfaces/__wralea__.py
+++ echap/src/alinea/echap_wralea/Interfaces/__wralea__.py
@@ -1,5 +1,5 @@
 
-# This file has been generated at Mon Apr 15 15:29:58 2013
+# This file has been generated at Tue Apr 16 11:49:47 2013
 
 from openalea.core import *
 
@@ -17,22 +17,7 @@ __institutes__ = ''
 __icon__ = ''
 
 
-__all__ = ['interfaces_nodes_local_microclimate', 'interfaces_nodes_pesticide_penetrated_decay', 'interfaces_nodes_pesticide_surfacic_decay', 'interfaces_nodes_pesticide_interception']
-
-
-
-interfaces_nodes_local_microclimate = Factory(name='local_microclimate',
-                authors=' (wralea authors)',
-                description='',
-                category='Unclassified',
-                nodemodule='interfaces_nodes',
-                nodeclass='local_microclimate',
-                inputs=[{'interface': None, 'name': 'g', 'value': None, 'desc': ''}, {'interface': None, 'name': 'scene', 'value': None, 'desc': ''}, {'interface': None, 'name': 'climate_model', 'value': None, 'desc': ''}, {'interface': IFloat, 'name': 'rain', 'value': 0, 'desc': ''}, {'interface': IStr, 'name': 'label', 'value': 'LeafElement', 'desc': ''}],
-                outputs=[{'interface': None, 'name': 'g', 'desc': ''}],
-                widgetmodule=None,
-                widgetclass=None,
-               )
-
+__all__ = ['interfaces_nodes_pesticide_penetrated_decay', 'interfaces_nodes_local_microclimate', 'interfaces_nodes_pesticide_surfacic_decay', 'interfaces_nodes_pesticide_interception']
 
 
 
@@ -42,8 +27,23 @@ interfaces_nodes_pesticide_penetrated_decay = Factory(name='pesticide_penetrated
                 category='Unclassified',
                 nodemodule='interfaces_nodes',
                 nodeclass='pesticide_penetrated_decay',
-                inputs=[{'interface': None, 'name': 'g', 'value': None, 'desc': 'MTG with penetrated doses of pesticide as property'}, {'interface': None, 'name': 'decay_model', 'value': None, 'desc': 'Model of penetrated pesticide decay'}, {'interface': IStr, 'name': 'label', 'value': None, 'desc': ''}, {'interface': IFloat, 'name': 'timestep', 'value': 1, 'desc': ''}],
+                inputs=[{'interface': None, 'name': 'g', 'value': None, 'desc': 'MTG with penetrated doses of pesticide as property'}, {'interface': None, 'name': 'decay_model', 'value': None, 'desc': 'Model of penetrated pesticide decay'}, {'interface': IStr, 'name': 'label', 'value': 'LeafElement', 'desc': ''}, {'interface': IInt, 'name': 'timestep', 'value': 1, 'desc': ''}],
                 outputs=[{'interface': None, 'name': 'g', 'desc': 'Update MTG with decay of the penetrated doses'}],
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
+interfaces_nodes_local_microclimate = Factory(name='local_microclimate',
+                authors=' (wralea authors)',
+                description='',
+                category='Unclassified',
+                nodemodule='interfaces_nodes',
+                nodeclass='local_microclimate',
+                inputs=[{'interface': None, 'name': 'g', 'value': None, 'desc': ''}, {'interface': None, 'name': 'scene', 'value': None, 'desc': ''}, {'interface': None, 'name': 'climate_model', 'value': None, 'desc': ''}, {'interface': IInt, 'name': 'rain', 'value': 0, 'desc': ''}, {'interface': IStr, 'name': 'label', 'value': 'LeafElement', 'desc': ''}],
+                outputs=[{'interface': None, 'name': 'g', 'desc': ''}],
                 widgetmodule=None,
                 widgetclass=None,
                )
@@ -57,7 +57,7 @@ interfaces_nodes_pesticide_surfacic_decay = Factory(name='pesticide_surfacic_dec
                 category='Unclassified',
                 nodemodule='interfaces_nodes',
                 nodeclass='pesticide_surfacic_decay',
-                inputs=[{'interface': None, 'name': 'g', 'value': None, 'desc': 'MTG'}, {'interface': None, 'name': 'decay_model', 'value': None, 'desc': 'Pearl decay model'}, {'interface': IStr, 'name': 'label', 'value': 'LeafElement', 'desc': 'Leaf element of MTG'}, {'interface': IFloat, 'name': 'timestep', 'value': 24, 'desc': ''}],
+                inputs=[{'interface': None, 'name': 'g', 'value': None, 'desc': 'MTG'}, {'interface': None, 'name': 'decay_model', 'value': None, 'desc': 'Pearl decay model'}, {'interface': IStr, 'name': 'label', 'value': 'LeafElement', 'desc': 'Leaf element of MTG'}, {'interface': IInt, 'name': 'timestep', 'value': 24, 'desc': ''}],
                 outputs=[{'interface': None, 'name': 'g', 'desc': 'New MTG with pesticide dose on leaf'}],
                 widgetmodule=None,
                 widgetclass=None,
