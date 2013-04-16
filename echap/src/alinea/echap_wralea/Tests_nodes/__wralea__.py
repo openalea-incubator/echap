4e8ad04914fc525db29e683f1123a7e6b344c39a
Romain Chapuis <chapuis.r@hotmail.com>
2013-04-16 15:54:36 +0000

    Add tests nodes
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@2613 0978ab36-7443-0410-8248-c2aa344a765a

---
+++ echap/src/alinea/echap_wralea/Tests_nodes/__wralea__.py
@@ -0,0 +1,38 @@
+
+# This file has been generated at Wed Apr 10 17:17:45 2013
+
+from openalea.core import *
+
+
+__name__ = 'Alinea.Echap.Tests_nodes'
+
+__editable__ = True
+__description__ = ''
+__license__ = 'CeCILL-C'
+__url__ = 'http://openalea.gforge.inria.fr'
+__alias__ = []
+__version__ = '0.8.0'
+__authors__ = ''
+__institutes__ = ''
+__icon__ = ''
+
+
+__all__ = ['tests_nodes_plot_pesticide']
+
+
+
+tests_nodes_plot_pesticide = Factory(name='plot_pesticide',
+                authors=' (wralea authors)',
+                description='',
+                category='Unclassified',
+                nodemodule='tests_nodes',
+                nodeclass='plot_pesticide',
+                inputs=[{'interface': None, 'name': 'g', 'value': None, 'desc': ''}],
+                outputs=[{'interface': None, 'name': 'OUT1', 'desc': ''}],
+                widgetmodule=None,
+                widgetclass=None,
+               )
+
+
+
+
---
+++ echap/src/alinea/echap_wralea/Tests_nodes/tests_nodes.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 08 16:06:53 2013
+
+@author: lepse
+"""
+
+from alinea.echap.tests_nodes import plot_pesticide
