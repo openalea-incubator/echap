9a4d50b99fbde79bbde19d47b0ff2c6ae87f92c6
Romain Chapuis <chapuis.r@hotmail.com>
2013-04-23 15:44:10 +0000

    update interception_leaf.py
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@2649 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/src/alinea/echap/interception_leaf.py
+++ echap/src/alinea/echap/interception_leaf.py
@@ -10,52 +10,57 @@ from numpy import recfromcsv
 from numpy import exp
 from math import radians, degrees, sin , cos
 
-# def products_from_csv(csvname, delimiter = ';') :
-    # """ 
-    # Read a csv of products names and import them in a dict.
-    # """
-    # tab = recfromcsv(csvname, delimiter = delimiter, case_sensitive = True)
-    # d = [dict(zip(tab.dtype.names, data)) for data in tab]
-    # b = dict([(k,{d:v}) for k,v in b.iteritems()])
-    # return b
-
-# productsDB = products_from_csv('E:/openaleapkg/echap/src/alinea/echap/products_names.csv')
+def products_from_csv(csvname, delimiter = ';'):
+    """ 
+    Read a csv of products parameters and import them in a dict.
+    Expected columns are :
+        - 'product' : commercial name of the product
+        - 'compound' : name of the active compound of the product
+        - 'dose' : dose of active compound in the product (g.l-1)    
+    """
+    tab = recfromcsv(csvname, delimiter = delimiter, case_sensitive = True)
+    d = {}
+    for i in range(0,len(tab['compound'])):
+        d[tab[i][0]] = {tab[i][1]:tab[i][2]}
+    return d
 
 
 def product_dose(product_name, dose, productsDB):
-    ''' 
+    """ 
     :Parameters:
     ----------
-            product_name: Commercial name of the product
-            dose: Application dose in l/ha
-            productsDB: Dict of products name and concentration of active compounds
+            product_name: commercial name of the product
+            dose: application dose in l.ha-1
+            productsDB: dict of products name, active compounds and concentration of active compounds
+
     :Returns:
     ----------
-            active_dose: Dose of active compound in g/m²
-    '''
-    for prod, sub in dict(productsDB).iteritems():
+            active_dose: Dose of active compound in g.m-2
+    """
+    for prod, sub in productsDB.iteritems():
         if prod == product_name:
             for compound_name, compound_dose in sub.iteritems():
                 active_dose = (compound_dose * dose) * 10**-4
     return compound_name, active_dose
 
-""" Implement pesticide interception_model using Caribu model
-"""
 
 def _vecteur_direction(elevation,azimuth):
     theta = radians(90 - elevation)
     phi = radians(azimuth)
     return sin(theta) * cos(phi),sin(theta) * sin(phi),  -cos(theta)
 
+
 def _emission_inv(elevation, product_name, dose, productsDB):
-    """ return energy of emmision for a source of a given direction and of a given energy received on a horizontal surface"""
+    """ return energy of emmision for a source of a given direction and of a given energy received on a horizontal surface """
     theta = radians(90 - elevation)
-    name, dose = product_dose(product_name, dose, productsDB) 
-    receive_dose = dose * abs(cos(theta))
-    return name, receive_dose
+    compound_name, active_dose = product_dose(product_name, dose, productsDB) 
+    receive_dose = active_dose * abs(cos(theta))
+    return compound_name, receive_dose
+
 
 def interception_dose(product_name, dose, productsDB, elevation, azimuth, scene):
-    name, receive_dose = _emission_inv(elevation, product_name, dose, productsDB)
+    """ Implement pesticide interception_model using Caribu model """
+    compound_name, receive_dose = _emission_inv(elevation, product_name, dose, productsDB)
     source = (receive_dose,(_vecteur_direction(elevation,azimuth))) # Quantité reçue
     c_scene = CaribuScene()    
     idmap = c_scene.add_Shapes(scene)      
@@ -63,12 +68,12 @@ def interception_dose(product_name, dose, productsDB, elevation, azimuth, scene)
     c_scene.getIncidentEnergy()
     output = c_scene.runCaribu(infinity=False)
     Einc = c_scene.output_by_id(output, idmap)['Einc']
-    return name, Einc 
+    return compound_name, Einc 
+
 
 class CaribuInterceptModel(object):
-    """ Adaptor for Caribu model compliying echap pesticide interception_model protocol 
-    """
-    def __init__(self, productsDB={'Ignite':{'Epoxiconazole':83}, 'Nevo':{'Chlorothalonil':375}, 'Caramba':{'Metconazole':60}}, elevation=90, azimuth=0): 
+    """ Adaptor for Caribu model compliying echap pesticide interception_model protocol """
+    def __init__(self, productsDB=products_from_csv('E:/openaleapkg/echap/src/alinea/echap/products_names.csv'), elevation=90, azimuth=0): 
         self.productsDB = productsDB
         self.elevation = elevation
         self.azimuth = azimuth
