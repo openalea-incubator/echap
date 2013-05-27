4bef827a13c99f212d733a5b60ebaf1bdacd97ac
Romain Chapuis <chapuis.r@hotmail.com>
2013-05-27 09:45:16 +0000

    add data folder with .csv files
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@2771 0978ab36-7443-0410-8248-c2aa344a765a

---
+++ echap/src/alinea/echap_wralea/Data/__wralea__.py
@@ -0,0 +1,44 @@
+
+# This file has been generated at Mon May 27 11:38:44 2013
+
+from openalea.core import *
+
+
+__name__ = 'Alinea.Echap.Data'
+
+__editable__ = True
+__description__ = ''
+__license__ = 'CeCILL-C'
+__url__ = 'http://openalea.gforge.inria.fr'
+__alias__ = []
+__version__ = '0.8.0'
+__authors__ = ''
+__institutes__ = ''
+__icon__ = ''
+
+
+__all__ = ['_214842992', '_213256784', '_214842960']
+
+
+_214842992 = DataFactory(name='products_parameters.csv',
+                    description='products_parameters',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_213256784 = DataFactory(name='products_names.csv',
+                    description='products_names',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+_214842960 = DataFactory(name='meteo01.csv',
+                    description='meteo01',
+                    editors=None,
+                    includes=None,
+                    )
+
+
+
