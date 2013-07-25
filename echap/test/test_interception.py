f623c404a5aae891d12dcb86853a9cf905159997
Romain Chapuis <chapuis.r@hotmail.com>
2013-07-25 07:35:30 +0000

    update test_interception
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@2967 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/test/test_interception.py
+++ echap/test/test_interception.py
@@ -1,6 +1,7 @@
 from alinea.echap.imports_echap import *
 
 
+############# Pesticide interception
 def test_intercept_caribu():
     g = adel_mtg()
     pest_calendar = {'datetime':["2000-10-01 01:00:00"], 'dose':[1.5], 'product_name':['Opus']}
@@ -18,4 +19,17 @@ def test_intercept_popdrops():
     tc = TimeControl(steps = 1, model = interception_model, start_date = "2000-10-01 01:00:00")
     time_control = tc.next()
     g = pesticide_interception(g, interception_model, time_control)
+    return g
+
+
+############# Rain interception
+def test_intercept_rain():    
+    g = adel_mtg()
+    meteo01_filepath = get_shared_data_path(['alinea/echap'], 'meteo01.csv')
+    t_deb = "2000-10-01 01:00:00"
+    weather = Weather(data_file=meteo01_filepath)
+    interception_model = RainInterceptionModel()
+    rain_timing = TimeControl(delay = 1, steps = 1, model = interception_model, weather = weather, start_date = t_deb)
+    time_control = rain_timing.next()
+    g = rain_interception(g, interception_model, time_control)
     return g
