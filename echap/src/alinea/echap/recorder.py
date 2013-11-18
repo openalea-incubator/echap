715d1b30bf514e2f5d2c3dad0d18f4b66308a40c
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2013-11-18 17:58:53 +0000

    setup simple recorder for leaf element
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@3285 0978ab36-7443-0410-8248-c2aa344a765a

---
+++ echap/src/alinea/echap/recorder.py
@@ -0,0 +1,22 @@
+import pandas
+
+class LeafElementRecorder:
+    def __init__(self):
+        self.data = {}
+        self.counts = 0
+        
+    def record(self, node, header):
+        if node.area > 0:
+            data = {}
+            properties = node.properties()
+            items = ['green_area']
+            for item in items:
+                if item in properties:
+                    data[item] = properties[item]
+            data.update(header)
+            self.data[self.counts] = data
+            self.counts += 1
+        
+    def save_records(self, path='./echap_outputs'):
+        d = pandas.DataFrame(self.data)
+        d.T.to_csv(path)
