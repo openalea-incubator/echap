a6b301cf1437bc08ada9e385d497c5480e85fd5e
Camille Chambon <cchambon@grignon.inra.fr>
2014-01-13 13:47:23 +0000

    Integration of plantgen in ECHAP.
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@3425 0978ab36-7443-0410-8248-c2aa344a765a

---
+++ echap/src/alinea/echap/share/data/Mercia_plantgen_inputs_MIN.py
@@ -0,0 +1,22 @@
+# This file stores a set of data which can be used by the function 
+# alinea.adel.plantgen.read_plantgen_inputs to define the inputs of the function 
+# alinea.adel.plantgen.gen_adel_input_data_from_min.
+# ATTENTION: this file is just an example and IS NOT portable. Please adapt it to 
+# your need manually or use the graphic node read_plantgen_inputs instead.
+plants_number = 203
+plants_density = 203
+decide_child_axis_probabilities = {'T0': 0.0277777777777778, 'T1': 1.000,
+                                   'T2': 0.833333333333333, 'T3': 0.75,
+                                   'T4': 0.472222222222222}
+MS_leaves_number_probabilities = {'10': 0.0,
+                                  '11': 0.354838709677419,
+                                  '12': 0.612903225806452,
+                                  '13': 0.032258064516129,
+                                  '14': 0.0}
+TT_bolting = 980
+final_axes_density = 444
+ears_density = 491
+GL_number = {1732.1: 4.03631578947369, 1818.05:3.01047368421053,
+             1959.55:1.90263157894737, 2108.65:0.0}
+delais_TT_stop_del_axis = 600
+TT_col_break = 0.0
