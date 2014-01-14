2365ba1685afb0cd60e4b57103859fb1f017cc08
Romain Chapuis <chapuis.r@hotmail.com>
2014-01-14 16:09:03 +0000

    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@3433 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/example/new_annual_loop.py
+++ echap/example/new_annual_loop.py
@@ -68,20 +68,21 @@ for i,controls in enumerate(zip(canopy_timing, doses_timing, rain_timing, pest_t
         print canopy_iter.value.index[-1]
         g = adel.grow(g, canopy_iter.value)
         _=rain_and_light_star(g, light_sectors = '1', domain=domain, convUnit=convUnit)
-        #_=update_lesions(g, canopy_iter.dt, True)   
-        #_=do_record(g, canopy_iter.value, recorder)
+        _=update_lesions(g, canopy_iter.dt, True)   
+        _=do_record(g, canopy_iter.value, recorder)
     if pest_iter:
         _=pesticide_intercept(g, pest_iter.value)
-        plot_pesticide(g)
+        #plot_pesticide(g)
     if doses_iter:
         print 'updte microclimate / doses...'
         _=microclimate_leaf(g, doses_iter.value, domain = domain, convUnit = convUnit)
         _=update_pesticides(g, doses_iter.value)
+        plot_pesticide(g)
         _=do_record(g, doses_iter.value, recorder, header={'iter':i, 'TT':adel.canopy_age})
     if rain_iter:
         wdata = rain_iter.value
-        #dispersion(g, wdata, domain, domain_area, convUnit)
-        #_=contamination(g,wdata, SspoSol, domain, domain_area, convUnit)
-        #_=infect(g, rain_iter.dt)
+        dispersion(g, wdata, domain, domain_area, convUnit)
+        _=contamination(g,wdata, SspoSol, domain, domain_area, convUnit)
+        _=infect(g, rain_iter.dt)
         
 recorder.save_records('test.csv')     
