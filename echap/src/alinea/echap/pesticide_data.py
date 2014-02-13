71fa822cffa00dd8a5c61fe88c1d6e021afd8d74
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2014-02-13 14:17:59 +0000

    update notebooks with modeling sprint results (to be clean-up)
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@3519 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/src/alinea/echap/pesticide_data.py
+++ echap/src/alinea/echap/pesticide_data.py
@@ -3,7 +3,8 @@
 
 # dosage (g.l-1) of active compounds in comercial products
 
-products_data={'Opus': {'Epoxiconazole': 125}, 
+products_data={'Opus': {'Epoxiconazole': 125},
+               'Opus_new': {'Epoxiconazole': 83},
                'Banko 500': {'Chlorothalonil': 500}}
 
 # application date and  application doses (l.ha-1)
@@ -50,26 +51,51 @@ milne_parameters = {'Epoxiconazole': {'Ae': 0.5,
 # 4.3E-5                CofDifWatRef (m2.d-1), taken as constant
 # CofDifAir             CofDifAirRef (m2.d-1)
 
-pearl_parameters = {'Epoxiconazole': {'MolarMass':329.8, 
+# by default, molecular parameters = pure active substance, DT50 = DT50 fromulated product (Nebila)
+
+# vitesse avance tracteur + volume to get ro : poitevin p17 pour tartrazine, quel est leulve pour opus ? cle meme que tartrazine ?
+# espacement entre buse = 
+
+pearl_parameters = {'Epoxiconazole_pure': {'MolarMass':329.8, 
                                       'VapourPressure':0.00001, 
                                       'TemVapPre':20, 
                                       'WatSolubility':7.1, 
                                       'TemWatSol':20, 
                                       'ThicknessLay':0.0006,
-                                      'DT50Pen':0.33,
-                                      'DT50Tra':0.433,
+                                      'DT50Pen':3.85,
+                                      'DT50Tra':1000,
                                       'CofDifAir':0.428,
                                       'TemDif':20,
-                                      'FacWas':0.0001}, 
+                                      'FacWas':0.0001},
+                    'Epoxiconazole': {'MolarMass':329.8, 
+                                      'VapourPressure':0.00001, 
+                                      'TemVapPre':20, 
+                                      'WatSolubility':7.1, 
+                                      'TemWatSol':20, 
+                                      'ThicknessLay':0.0006,
+                                      'DT50Pen':1.44,
+                                      'DT50Tra':1000,# inifite as k almost zero (Pierre)
+                                      'CofDifAir':0.428,
+                                      'TemDif':20,
+                                      'FacWas':0.0001},
                     'Chlorothalonil': {'MolarMass':265.9,
                                        'VapourPressure':0.0000762,
                                        'TemVapPre':20,
                                        'WatSolubility':0.81,
                                        'TemWatSol':20, 
                                        'ThicknessLay':0.0006,
-                                       'DT50Pen':0.14,
-                                       'DT50Tra':0.23,
+                                       'DT50Pen':3,
+                                       'DT50Tra':2.1,
                                        'CofDifAir':0.428,
                                        'TemDif':20,
                                        'FacWas':0.0001}
                     }
+                    
+#granulometry : 
+# Buse : type of nozzle          
+# Prouit : product (tartrazine for validation, and opus pour simulation)
+# Vdep : nozzle displacement (covariable, mm.s-1) : used bothe speed (5/10 or only one)
+# Vitesse : drop speed (m.s-1)
+# Diam : drop diameter (micro.m)
+# xc : distance to the nozzle center (mm)
+# Vitesse          
