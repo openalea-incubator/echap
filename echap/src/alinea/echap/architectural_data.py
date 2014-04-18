b20c1a3396b540b30ee84c1e3ff66ec06e0c1efd
Romain Chapuis <chapuis.r@hotmail.com>
2014-04-18 11:06:37 +0000

    add data and calibration for 2010-2011
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@4055 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/src/alinea/echap/architectural_data.py
+++ echap/src/alinea/echap/architectural_data.py
@@ -4,62 +4,144 @@ from openalea.deploy.shared_data import shared_data
 import alinea.echap
 from alinea.adel.plantgen.plantgen import read_plantgen_inputs
 
+#-------------------------------------------------------------------------------  
 def Mercia_2010_plantgen():
     dynT = shared_data(alinea.echap, 'Mercia_dynT_user.csv')
     dimT = shared_data(alinea.echap, 'Mercia_dimT_user.csv')
     inputs = shared_data(alinea.echap, 'Mercia_plantgen_inputs_MIN.py')
+    #dynT = shared_data(alinea.echap, 'Tremie1_dynT_user.csv')
+    #dimT = shared_data(alinea.echap, 'Tremie1_dimT_user.csv')
+    #inputs = shared_data(alinea.echap, 'Tremie1_plantgen_inputs_MINnew.py')
     d={}
     d['dynT_user'], d['dimT_user'], d['plants_number'],d['plants_density'], d['decide_child_axis_probabilities'], d['MS_leaves_number_probabilities'], d['ears_density'], d['GL_number'], d['delais_TT_stop_del_axis'], d['TT_col_break'],d['inner_params'] =  read_plantgen_inputs(inputs, dynT, dimT)
     return d
-
-        
-def HS_data_2010():
+def Tremie_2011_plantgen():
+    dynT = shared_data(alinea.echap, 'Tremie1_dynT_user.csv')
+    dimT = shared_data(alinea.echap, 'Tremie1_dimT_user.csv')
+    inputs = shared_data(alinea.echap, 'Tremie1_plantgen_inputs_MINnew.py')
+    d={}
+    d['dynT_user'], d['dimT_user'], d['plants_number'],d['plants_density'], d['decide_child_axis_probabilities'], d['MS_leaves_number_probabilities'], d['ears_density'], d['GL_number'], d['delais_TT_stop_del_axis'], d['TT_col_break'],d['inner_params'] =  read_plantgen_inputs(inputs, dynT, dimT)
+    return d
+    
+def HS_data():
     fn = shared_data(alinea.echap, 'HS_data_Mercia_Rht3_2010_2011.csv')
+    #fn = shared_data(alinea.echap, 'HS_data_Tremie1_2011_2012.csv')
     data = pandas.read_csv(fn,decimal=',',sep='\t')
     grouped = data.groupby(['variety'],as_index=False)
     return grouped.aggregate('mean')
     
-def Tillering_data_2010():
-    """ Tillering data for Boigneville 2010-2011
+#-------------------------------------------------------------------------------   
+def _maxna(x):
+    m = x.max()
+    if any(x.isnull()) and m == 0:
+        m = None
+    return m
     
-    Expected data are :
-    - estimates of plant density at emergence
-    - estimates of ear density at harvest (all axes)
+def diff(edata, s):
+    e1 = edata.head(0)
+    e2 = s.head(0) 
+    e = list(set(e1) - set(e2))
+    i = 0
+    while i<(len(e)-1):
+        s [e[i]] = None
+        i = i + 1
+    return s
+    
+def emis(data, d, n):
+    res = d.ix[:,'Nff':] / n.ix[:,'Nff':] 
+    for c in d.ix[:,'Nff':].columns.values:
+        d.ix[:,c] = res.ix[:,c]
+    # update TPE and compute MB and TT
+    d['TPE'] = d.ix[:,'TC':'T5'].sum(axis=1)
+    edata = data[data['Date'] >= 6]
+    edata = edata.reset_index()
+    grouped = edata.groupby(['Var','Date'],as_index=False)
+    return grouped
+    
+#-------------------------------------------------------------------------------  
+def Tillering_data_Mercia_Rht3_2010_2011():
+    """Tillering data for Boigneville 2010-2011
+    Found data are (id_data refers to column in data sythesis):
+    - estimates of plant density at emergence (id Archi12)
+    - estimates of ear density at harvest (all axes, id Archi10)
     - estimates of the number of elongated internodes on main stems
-    - estimates of tiller dynamics, with three kind of data : 
+    - estimates of tiller dynamics, with three kind of data :
         - presence/absence of living mainstem (column MS) and number of leaf emited
         - presence/absence of primary tiller (column Tc-> Tn) whatever the state (dead or alive), and/or  total number of primary tillers (column TPE) and secondary tillers emited (dead or alive, column TSE). These data are used for fitting emission probabilities
-        - estimation of total living tillers (column TT) at the end from the counting of tillers that are alive or that bears an ear """
+        - estimation of total living tillers (column TT) at the end from the counting of tillers that are alive or that bears an ear 
+    Others data :
+    - Number of secondary tiller to date 3 (Archi33)
+    Notes :
+    - No data to date 5
+    - Missing raw data for plant counts at emergence (Archi7, used Archi12 instead)"""
     
     # summarise tillering data to mean number of tiller emited per plant (columns Tc->T5), mean number of leaves, and total tillers and MS present per plant at the end (column TT and MB)
+    # this table was contrusted using Archi11,Archi19, Archi33
     fn = shared_data(alinea.echap, 'Tillering_data_Mercia_Rht3_2010_2011.csv')
     data = pandas.read_csv(fn,decimal=',',sep='\t')
     # compute emmission probas of primary axis/ emission per plant of sencondary axis 
     edata = data[data['Date'] < 6]
     edata = edata.reset_index()
     grouped = edata.groupby(['Var', 'N'],as_index=False)
-    def _maxna(x):
-        m = x.max()
-        if any(x.isnull()) and m == 0:
-            m = None
-        return m
     d = grouped.agg(_maxna)
     d = d.reset_index()
+    #---
+    de = d.fillna(0)
+    grouped1 = de.groupby(['Var','Date'],as_index=False)
+    #---
     grouped = d.groupby(['Var','Date'],as_index=False)
     s = grouped.agg('sum')
-    n = grouped.agg(lambda x: x.dropna().count())
+    n = grouped1.agg(lambda x: float(x.dropna().count())) 
+    diff(edata, s)
     d = s
-    d.ix[:,'Nff':] = 1.0 * d.ix[:,'Nff':] / n.ix[:,'Nff':]
-    # update TPE and compute MB and TT
-    d['TPE'] = d.ix[:,'TC':'T5'].sum(axis=1)
-    edata = data[data['Date'] >= 6]
-    edata = edata.reset_index()
-    grouped = edata.groupby(['Var','Date'],as_index=False)
+    emis(data, d, n)
     s = grouped.agg('sum')
     n = grouped.agg(lambda x: x.dropna().count())
     d['MB'] = 1.0 * s['MB'] / n['MB']
     d['TT'] = 1.0 * s['TT'] / n['TT']
-    obs = {'plant_density_at_emergence' : {'Mercia':203, 'Rht3':211},
-            'ear_density_at_harvest' : {'Mercia': 444, 'Rht3' : 384}, 
+    obs = {'plant_density_at_emergence' : {'Mercia': 203, 'Rht3': 211}, 'ear_density_at_harvest' : {'Mercia': 444, 'Rht3' : 384}, 
+            'tillering' : d}
+    return obs
+
+def Tillering_data_Tremie1_2011_2012():
+    """Tillering data for Boigneville 2011-2012
+    Expected data are :
+    - estimates of plant density at emergence (id Archi1)
+    - estimates of ear density at harvest (all axes) (id Archi16)
+    - estimates of the number of elongated internodes on main stems
+    - estimates of tiller dynamics, with three kind of data : 
+        - presence/absence of living mainstem (column MS) and number of leaf emited
+        - presence/absence of primary tiller (column Tc-> Tn) whatever the state (dead or alive), and/or  total number of primary tillers (column TPE) and secondary tillers emited (dead or alive, column TSE). These data are used for fitting emission probabilities
+        - estimation of total living tillers (column TT) at the end from the counting of tillers that are alive or that bears an ear
+    Others data :
+    - Number of secondary tiller to date 1 
+    Notes :
+    - No data to date 2, 4 & 5"""
+    
+    # summarise tillering data to mean number of tiller emited per plant (columns Tc->T7), mean number of leaves, and total tillers and MS present per plant at the end (column TT and MB)
+    # this table was contrusted using Archi2, Archi3, Archi14, Archi15
+    fn = shared_data(alinea.echap, 'Tillering_data_Tremie1_2011_2012.csv')
+    data = pandas.read_csv(fn,decimal=',',sep='\t')
+    # compute emmission probas of primary axis/ emission per plant of sencondary axis 
+    edata = data[data['Date'] < 6]
+    edata = edata.reset_index()
+    grouped = edata.groupby(['Var', 'N'],as_index=False)
+    d = grouped.agg(_maxna)
+    d = d.reset_index()
+    #---
+    de = d.fillna(0)
+    grouped1 = de.groupby(['Var','Date'],as_index=False)
+    #---
+    grouped = d.groupby(['Var','Date'],as_index=False)
+    s = grouped.agg('sum')
+    n = grouped1.agg(lambda x: float(x.dropna().count())) 
+    diff(edata, s)
+    d = s
+    emis(data, d, n)
+    s = grouped.agg('sum')
+    n = grouped.agg(lambda x: x.dropna().count())
+    d['MB'] = 1.0 * s['MB'] / n['MB']
+    d['TT'] = 1.0 * s['TT'] / n['TT']
+    obs = {'plant_density_at_emergence' : {'Tremie': 278.55}, 'ear_density_at_harvest' : {'Tremie': 491}, 
             'tillering' : d}
     return obs
--- echap/test/FunctionalTests/Architecture/TestCalibration.py
+++ echap/test/FunctionalTests/Architecture/TestCalibration.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from alinea.adel.WheatTillering import WheatTillering
 import alinea.echap.architectural_data as archidb
 
+import multiprocessing as mp
+
 plt.ion()
 
 def test_axis_dynamics():
@@ -31,15 +33,19 @@ def test_axis_dynamics():
     
     # todo : add comparison to primary emission
     
-    #test plantgen fit
+    #test plantgen fit&
     
     fit = m.axis_dynamics(plant_density = plant_density)
+    
     new_fit = new_m.axis_dynamics(plant_density = float(tdata['ear_density_at_harvest']['Mercia']) / new_m.ears_per_plant)
     fit.plot('HS', ['total','primary','others'], style=['--r','--g','--b'])
     new_fit.plot('HS', ['total','primary','others'],style=['-r','-g','-b'])
     plt.plot([1,13],[tdata['plant_density_at_emergence']['Mercia'],tdata['ear_density_at_harvest']['Mercia']] ,'ob')
     
-def test_adel():
+def test_adel(TT_stop):
+    #var = 'Mercia'
+    var = 'Rht3'
+    #var = 'Tremie'
     from alinea.echap.macros_annual_loop import setup_canopy
     from alinea.adel.plantgen.plantgen_interface import gen_adel_input_data,plantgen2adel
     from alinea.adel.stand.stand import agronomicplot
@@ -48,19 +54,66 @@ def test_adel():
     from alinea.adel.postprocessing import axis_statistics, plot_statistics 
     
     pgen = archidb.Mercia_2010_plantgen()
+    #pgen = archidb.Tremie_2011_plantgen()
+    tdata = archidb.Tillering_data_Mercia_Rht3_2010_2011()
+    #tdata = archidb.Tillering_data_Tremie1_2011_2012()
+    obs = tdata['tillering']
+    obs = obs[obs.Var==var]
+    #primary_proba={'T2': obs['MB'].values + obs['TC'].values}#handle damages to MB and simplify TC
+    primary_proba={}
+    for w in ('T1','T2','T3','T4'):
+        primary_proba[w] = obs[w].values
+    ears_per_plant = obs['MB'].values + obs['TT'].values
+    nff = float(obs['Nff'].values)
+    new_m = WheatTillering(primary_tiller_probabilities=primary_proba, ears_per_plant = ears_per_plant, nff=nff)
+    #update avec les probas primares de Tillering data
+    pgen['decide_child_axis_probabilities'] = primary_proba
+    # replace plant_number par eardensity / ear_per_plant, avec earsperplant =tillering data['MB'].values + Tillering data['TT'].values
+    pgen['plants_number'] = float(tdata['ear_density_at_harvest'][var]) / new_m.ears_per_plant
+    #
+    pgen['delais_TT_stop_del_axis'] -= TT_stop
+    tx = pgen['dynT_user'].a_cohort[0]
     axeT_, dimT_, phenT_, _, _, _, _, _, _, _, _ = gen_adel_input_data(**pgen)
     axeT, dimT, phenT = plantgen2adel(axeT_, dimT_, phenT_)
     devT = devCsv(axeT, dimT, phenT)
 
-    nplants, positions, domain, domain_area, convUnit = agronomicplot(length=0.2, 
-                                                            width=0.15, 
+    nplants, positions, domain, domain_area, convUnit = agronomicplot(length=0.5, 
+                                                            width=0.5, 
                                                             sowing_density=pgen['plants_density'], 
                                                             plant_density=pgen['plants_number'],
                                                             inter_row=0.125)
     adel = AdelWheat(nplants=nplants, positions = positions, nsect=1, devT=devT, seed= 1, sample='random')
-    dd = range(0,1500,100)
+    dd = range(0,2000,100)
     outs = [adel.get_exposed_areas(g, convert=True) for g in (adel.setup_canopy(age) for age in dd)]
-    out = reduce(lambda x,y: pandas.concat([x,y],ignore_index=True), outs)
+    new_outs = [df for df in outs if not df.empty]
+    out = reduce(lambda x,y: pandas.concat([x,y],ignore_index=True), new_outs)
     axstat = axis_statistics(out, domain_area, convUnit)    
-    res =  plot_statistics(axstat, nplants, domain_area)
-    return res
+    res.var.TT_stop =  plot_statistics(axstat, nplants, domain_area)
+    return res.var.TT_stop, tx
+    #res['HS'] = res.ThermalTime*tx
+    #print res.plot('HS','PAI_vert',color='b')
+
+def compare_LAI():
+    sim = test_adel()
+    #Mercia
+    #obs = pandas.DataFrame({'TT':[1137,1281,1796],'PAI':[3.27,4.03,4.05]})
+    #Rht3
+    obs = pandas.DataFrame({'TT':[1137,1281,1796],'PAI':[3.40,3.96,3.45]})
+    #Tremie2012
+    #obs = pandas.DataFrame({'TT':[923,1260,1550],'PAI':[1.1,4.31,4.43]})
+    #Tremie2013
+    #obs = pandas.DataFrame({'TT':[917.5,1018.3,1377,1612.25],'PAI':[1.73,2.7,2.78,1.95]})
+    #sim.plot('ThermalTime','PAI_vert')
+    #obs.plot('TT','PAI',style='o')
+    sim['HS'] = sim.ThermalTime*tx
+    sim.plot('HS','PAI_vert',color='g')
+    obs['HS'] = obs.TT*tx
+    obs.plot('HS','PAI',style='o',color='r')
+
+TT_stop_list=[0]
+var = 'Rht3'
+if __name__=="__main__":
+    TT_stop = [TT_stop for TT_stop in TT_stop_list]
+    print TT_stop
+    p = mp.Pool(4)
+    p.map(test_adel,TT_stop)
