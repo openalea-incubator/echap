2acae165b803af795999e096682c61be62e65d35
Romain Chapuis <chapuis.r@hotmail.com>
2014-04-22 15:53:42 +0000

    add example for coverFraction computing
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@4065 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/test/FunctionalTests/Architecture/TestCalibration.py
+++ echap/test/FunctionalTests/Architecture/TestCalibration.py
@@ -1,11 +1,13 @@
 import pandas
 import numpy
+import os
 import matplotlib.pyplot as plt
 
 from alinea.adel.WheatTillering import WheatTillering
 import alinea.echap.architectural_data as archidb
 
-import multiprocessing as mp
+from multiprocessing import Pool,freeze_support
+import itertools
 
 plt.ion()
 
@@ -41,18 +43,12 @@ def test_axis_dynamics():
     fit.plot('HS', ['total','primary','others'], style=['--r','--g','--b'])
     new_fit.plot('HS', ['total','primary','others'],style=['-r','-g','-b'])
     plt.plot([1,13],[tdata['plant_density_at_emergence']['Mercia'],tdata['ear_density_at_harvest']['Mercia']] ,'ob')
-    
-def test_adel(TT_stop):
-    #var = 'Mercia'
-    var = 'Rht3'
-    #var = 'Tremie'
-    from alinea.echap.macros_annual_loop import setup_canopy
+
+def setAdel(TT_stop, var):
     from alinea.adel.plantgen.plantgen_interface import gen_adel_input_data,plantgen2adel
     from alinea.adel.stand.stand import agronomicplot
     from alinea.adel.astk_interface import AdelWheat
     from alinea.adel.AdelR import devCsv
-    from alinea.adel.postprocessing import axis_statistics, plot_statistics 
-    
     pgen = archidb.Mercia_2010_plantgen()
     #pgen = archidb.Tremie_2011_plantgen()
     tdata = archidb.Tillering_data_Mercia_Rht3_2010_2011()
@@ -82,14 +78,27 @@ def test_adel(TT_stop):
                                                             sowing_density=pgen['plants_density'], 
                                                             plant_density=pgen['plants_number'],
                                                             inter_row=0.125)
-    adel = AdelWheat(nplants=nplants, positions = positions, nsect=1, devT=devT, seed= 1, sample='random')
+    adel = AdelWheat(nplants=nplants, positions = positions, nsect=10, devT=devT, seed= 1, sample='random')
+    return adel, nplants, positions, domain, domain_area, convUnit
+    
+
+    
+def sample_adel():
+    adel, nplants, positions, domain, domain_area, convUnit = setAdel(0, 'Mercia')
+    return adel, domain
+
+def simLAI(TT_stop, var):
+    
+    from alinea.adel.postprocessing import axis_statistics, plot_statistics 
+    
+    adel, nplants, positions, domain, domain_area, convUnit = setAdel(TT_stop,var)
     dd = range(0,2000,100)
     outs = [adel.get_exposed_areas(g, convert=True) for g in (adel.setup_canopy(age) for age in dd)]
     new_outs = [df for df in outs if not df.empty]
     out = reduce(lambda x,y: pandas.concat([x,y],ignore_index=True), new_outs)
     axstat = axis_statistics(out, domain_area, convUnit)    
-    res.var.TT_stop =  plot_statistics(axstat, nplants, domain_area)
-    return res.var.TT_stop, tx
+    res =  plot_statistics(axstat, nplants, domain_area)
+    return res, tx
     #res['HS'] = res.ThermalTime*tx
     #print res.plot('HS','PAI_vert',color='b')
 
@@ -110,10 +119,57 @@ def compare_LAI():
     obs['HS'] = obs.TT*tx
     obs.plot('HS','PAI',style='o',color='r')
 
-TT_stop_list=[0]
-var = 'Rht3'
-if __name__=="__main__":
-    TT_stop = [TT_stop for TT_stop in TT_stop_list]
-    print TT_stop
-    p = mp.Pool(4)
-    p.map(test_adel,TT_stop)
+def draft_TC(adel, domain,thermal_time, pov_dirpath='.'):
+    from alinea.adel.povray import povray
+    from alinea.adel.mtg_interpreter import plot3d   
+    
+    g =adel.setup_canopy(thermal_time)
+    # colors
+    colors_def = {'green':[0, 255, 0], 'red' : [255, 0, 0]}
+    greeness = g.property('is_green')
+    colors = {k:colors_def['green'] if greeness[k] else colors_def['red'] for k in greeness}
+    # scene
+    scene = plot3d(g, colors=colors)
+    # plot3D
+    # povray
+    pov_filename = os.path.join(pov_dirpath, '%s%s%s' % ('scene_', thermal_time, '.pov'))
+    povray_image_filepath, stand_box_image_filepath = povray.povray(scene, pov_filename, 200.0, 50, 4288, 2848, domain, 0, 0, 'perspective', False, 'povray')
+    # genFilepath
+    from alinea.adel.povray import post_processing
+    outpath = './counts.txt'
+    post_processing.count_pixels(povray_image_filepath, stand_box_image_filepath, colors_def.values(), outpath)
+    return povray_image_filepath, stand_box_image_filepath, colors_def
+    
+def color_count(image,RGBcolor = (0,255,0)):
+    import cv2
+    BGRcolor = numpy.array(RGBcolor[::-1])
+    res = cv2.inRange(image, BGRcolor, BGRcolor)
+    return res.sum() / 255.
+    
+def count_pixels(povray_image_filepath, stand_box_image_filepath, colors_def):
+    import cv2
+    im = cv2.imread(povray_image_filepath)
+    box = cv2.imread(stand_box_image_filepath)
+    mask = numpy.uint8(box[:,:,0] / box.max() * 255)
+    total = mask.sum() / 255.
+    masked=cv2.bitwise_and(im,im,mask=mask)
+    return {k:color_count(masked,v) / total for k,v in colors_def.iteritems()}
+    
+    
+    
+
+    
+    
+def func_star(a_b):
+    return test_adel(*a_b)
+def main():
+    p = Pool()
+    TT_stop=[0,600,400,200]
+    #var = "Mercia"
+    var = "Rht3"
+    #var = "Tremie"
+    p.map(func_star, itertools.izip(TT_stop, itertools.repeat(var)))
+# if __name__=="__main__":
+    # freeze_support()
+    # main()
+
