1cf525e0367ee47c15d27f68ac2c272181115685
Stéphanie Pointet <stephanie.pointet@supagro.inra.fr>
2014-06-30 08:48:32 +0000

    Test methode LAI (testCalibration / grow_dd) + recorder_f - Plot de comparaison
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@4313 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/example/test_adel.py
+++ echap/example/test_adel.py
@@ -31,7 +31,7 @@ def sim_LAI_methode1(adel, domain_area, convUnit, nplants):
     res =  plot_statistics(axstat, nplants, domain_area)
     return res
 
-def sim_LAI_methode2(adel, domain_area, convUnit, nplants):
+def sim_LAI_methode2(adel, domain_area, convUnit, nplants, adelpars):
     delta_dd = 100; dd_start = 0; dd_end = 2300
     dd = range(dd_start, dd_end, delta_dd)
     
@@ -39,7 +39,7 @@ def sim_LAI_methode2(adel, domain_area, convUnit, nplants):
     gouts = [adel.get_exposed_areas(g, convert=True)]
     #gouts = adel.get_exposed_areas(g, convert=True)
     for i in range((dd_end - dd_start) / delta_dd):
-        g = adel.grow_dd(g, delta_dd)
+        g = adel.grow_dd(g, delta_dd, adelpars)
         gouts.append(adel.get_exposed_areas(g, convert=True))
         #gouts = gouts.append(adel.get_exposed_areas(g, convert=True)) 
     new_gouts = [df for df in gouts if not df.empty]
@@ -91,10 +91,11 @@ def plot_LAI(name='Mercia', dTT_stop=0, original=False, n=30):
     pgen, adel, domain, domain_area, convUnit, nplants = get_reconstruction(name, nplants = n, dTT_stop=dTT_stop, as_pgen=original)
     tx = pgen['dynT_user'].a_cohort[0]
     
+    adelpars = {'senescence_leaf_shrink' : 1, 'startLeaf' : -0.4, 'endLeaf' : 1.6, 'endLeaf1': 1.6, 'stemLeaf' : 1.2, 'epsillon' : 1e-6}
+    
     # PLOT SIM LAI TESTCALIBRATION.PY ET AUTRE METHODE GROW_DD
-    '''
     outs = sim_LAI_methode1(adel, domain_area, convUnit, nplants)
-    gouts = sim_LAI_methode2(adel, domain_area, convUnit, nplants)
+    gouts = sim_LAI_methode2(adel, domain_area, convUnit, nplants, adelpars)
     #print outs
     #print gouts
     
@@ -107,12 +108,11 @@ def plot_LAI(name='Mercia', dTT_stop=0, original=False, n=30):
     gouts.plot('HS', 'LAI_vert', color='r')
     outs.plot('HS', 'LAI_tot', color='m')
     
-    plt.show()'''
+    plt.show()
     
     # RECORDER
     weather = Boigneville_2010_2011()
-    seq = pandas.date_range(start = "2010-10-15", periods = 6000, freq = 'H')  # 5000 pour le cycle complet
-    adelpars = {'senescence_leaf_shrink' : 1, 'startLeaf' : -0.4, 'endLeaf' : 1.6, 'endLeaf1': 1.6, 'stemLeaf' : 1.2, 'epsillon' : 1e-6}
+    seq = pandas.date_range(start = "2010-10-15", periods = 6000, freq = 'H')  # 5000 pour le cycle complet 
     rec = recorder_f(weather, adel, domain, convUnit, seq, adelpars)
     # concordance date AAAA-MM-JJ en TT
     bid = thermal_time(seq, weather.data)
