0ee5fc80404e2c90a8c82026295446135f3a4382
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2014-10-03 07:35:08 +0000

    add max_order option
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@4665 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/src/alinea/echap/architectural_reconstructions.py
+++ echap/src/alinea/echap/architectural_reconstructions.py
@@ -145,14 +145,14 @@ if run_plots:
 #
 # Fit tillering
 #
-def _tfit(tdb, delta_stop_del, n_elongated_internode):
+def _tfit(tdb, delta_stop_del, n_elongated_internode, max_order):
     ms_nff_probas = tdb['nff_probabilities']
     nff = sum([int(k)*v for k,v in ms_nff_probas.iteritems()]) 
     ears_per_plant = tdb['ears_per_plant']
     primary_emission = {k:v for k,v in tdb['emission_probabilities'].iteritems() if v > 0}
-    return WheatTillering(primary_tiller_probabilities=primary_emission, ears_per_plant = ears_per_plant, nff=nff,delta_stop_del=delta_stop_del,n_elongated_internode = n_elongated_internode)
+    return WheatTillering(primary_tiller_probabilities=primary_emission, ears_per_plant = ears_per_plant, nff=nff,delta_stop_del=delta_stop_del,n_elongated_internode = n_elongated_internode, max_order=max_order)
     
-def tillering_fits(delta_stop_del=2.8, n_elongated_internode={'Mercia':4, 'Rht3':4, 'Tremie12': 5, 'Tremie13':4} ):
+def tillering_fits(delta_stop_del=2.8, n_elongated_internode={'Mercia':4, 'Rht3':4, 'Tremie12': 6, 'Tremie13':4} , max_order=None):
         
     tdb = archidb.Tillering_data()
     pdata = archidb.Plot_data_Tremie_2012_2013()
@@ -164,14 +164,14 @@ def tillering_fits(delta_stop_del=2.8, n_elongated_internode={'Mercia':4, 'Rht3'
     if not isinstance(n_elongated_internode,dict):
         n_elongated_internode = {k:n_elongated_internode for k in tdb}   
     
-    t_fits={k: _tfit(tdb[k], delta_stop_del=delta_stop_del[k],n_elongated_internode=n_elongated_internode[k]) for k in tdb}
+    t_fits={k: _tfit(tdb[k], delta_stop_del=delta_stop_del[k],n_elongated_internode=n_elongated_internode[k], max_order=max_order) for k in tdb}
 
     return t_fits
 
 if run_plots:
     delta_stop_del = 2.8
     #delta_stop_del={'Mercia':3.5, 'Rht3':2.5, 'Tremie12': 0.5, 'Tremie13':2.5}#handle freezing effect on Tremie12
-    fits = tillering_fits(delta_stop_del=delta_stop_del)
+    fits = tillering_fits(delta_stop_del=delta_stop_del, max_order=None)
     obs = archidb.tillers_per_plant()
     archi_plot.multi_plot_tillering(obs, fits, delta_stop_del)
    
