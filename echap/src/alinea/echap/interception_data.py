5aa105cc6ee52eaf5d3bebcbc9f02741cd59a93f
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2014-10-22 13:43:03 +0000

    add dye interception data
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@4783 0978ab36-7443-0410-8248-c2aa344a765a

---
+++ echap/src/alinea/echap/interception_data.py
@@ -0,0 +1,19 @@
+''' Data for pesticide/dye intercption experiments
+'''
+
+import pandas
+
+def dye_interception():
+    """ Dye interception data
+    
+    data are compiled mean/sd from dye interception.
+    
+    units : deposit per leaf (g per g.cm-2 applied)
+    
+    All volume mixed
+    
+    Raw data are in ECHAP_ARVALIS/Interception/Donnees brutes 3 annee
+    
+    original units : microg. per g.ha-1 (converted in g : g.cm-2 by multiplying by 100)
+    """
+    
