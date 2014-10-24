e8d1cd36262257ac6fff24f0919c4488f44cfd0e
Stéphanie Pointet <stephanie.pointet@supagro.inra.fr>
2014-10-24 12:59:49 +0000

    Ajout HS des dates supplémentaires pour Tremie12 et Tremie13, necessaire pour les plots dans dye_interception
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@4800 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/src/alinea/echap/interception_data.py
+++ echap/src/alinea/echap/interception_data.py
@@ -1,4 +1,4 @@
-''' Data for pesticide/dye intercption experiments
+''' Data for pesticide/dye interception experiments
 '''
 
 import pandas
@@ -38,9 +38,13 @@ def dye_applications():
                       'T2': ('2011-05-11', 12.8)},#HS not measured but estimated from HSconvert
             'Rht3': {'T1':('2011-04-19', 9.15), #mesure HS le 18
                      'T2':('2011-05-11', 12.48)},# Hsconvert estimation
-            'Tremie12': {'T1':('2012-04-11', 10.98),
+            'Tremie12': {'date1':('2012-03-09', 7.55),#ajout date scan1
+                     'date2':('2012-04-02', 10.15),#ajout date scan2
+                     'T1':('2012-04-11', 10.98),
                      'T2':('2012-05-09', 12.63)},
-            'Tremie13': {'T1':('2013-04-25', 8.7),#HSconvert estimation
-                     'T2':('2013-05-17', 11.04) } #HS convert   etimation                
+            'Tremie13': {'date1':('2013-04-22', 8.36),#HSconvert estimation
+                     'T1':('2013-04-25', 8.7),#HSconvert estimation
+                     'date2':('2013-05-03', 9.7),#HS mesure le 02
+                     'T2':('2013-05-17', 11.04) } #HS convert estimation                
     }
     return apps
