9ec43970ba76b06bee7af94b2790cd52cb717a57
Guillaume Garin <guillaume.garin@itk.fr>
2015-03-23 08:53:33 +0000

    Try to overpass bug tillering in plantgen
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@5148 0978ab36-7443-0410-8248-c2aa344a765a

---
+++ echap/test/FunctionalTests/Architecture/test_bug_reconstructions.py
@@ -0,0 +1,21 @@
+from alinea.echap.architectural_reconstructions import EchapReconstructions
+variety = 'Tremie12'
+nplants = 10
+nsect = 7
+disc_level = 5
+reconst = EchapReconstructions()
+adel = None
+tries = 0.
+while adel is None:
+    tries += 1
+    print '='*80
+    print tries
+    print '='*80
+    try:
+        adel = reconst.get_reconstruction(name=variety, nplants = nplants, nsect = nsect, 
+                                            disc_level = disc_level, aspect = 'line')
+        print '='*80
+        print "C EST BON "
+        print '='*80
+    except:
+        pass
