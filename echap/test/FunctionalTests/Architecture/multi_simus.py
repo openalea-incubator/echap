7cbd4faca5625661d6579f30f4a48a0c897c8371
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2015-03-31 21:01:58 +0000

    fix import error
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@5181 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/test/FunctionalTests/Architecture/multi_simus.py
+++ echap/test/FunctionalTests/Architecture/multi_simus.py
@@ -3,11 +3,11 @@
 
 from openalea.multiprocessing.parallel import pymap
 from multiprocessing import cpu_count
-from evaluation_canopy_properties import *
+from alinea.echap.evaluation_canopy_properties import *
 
-def run_variety(variety = 'Tremie12'):
+def run_variety(variety = 'Tremie12', reset_reconst=False):
     run_and_save_multi_simu(variety = variety, nplants = 30, age_range = [400, 2600],
-                            time_steps = [20, 100], nrep = 1)
+                            time_steps = [20, 100], nrep = 1, reset_reconst=True)
 
 # TODO : Set cmdlines that manage several jobs on One open Povray
 # if __name__ == '__main__':
@@ -17,3 +17,4 @@ def run_variety(variety = 'Tremie12'):
     
 for variety in ['Mercia', 'Rht3', 'Tremie12', 'Tremie13']:
     run_variety(variety)
+    #run_variety(variety,reset_reconst=True)
