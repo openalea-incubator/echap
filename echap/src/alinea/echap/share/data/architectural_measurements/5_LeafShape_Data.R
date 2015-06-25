39b494d3fd01018a4788204db569d0654cf18a61
Christian Fournier <Christian.Fournier@supagro.inra.fr>
2015-06-25 12:57:08 +0000

    script for estimating rank, relative ranktop and ranktop in curvature database
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/openaleapkg/trunk@5356 0978ab36-7443-0410-8248-c2aa344a765a

--- echap/src/alinea/echap/share/data/architectural_measurements/5_LeafShape_Data.R
+++ echap/src/alinea/echap/share/data/architectural_measurements/5_LeafShape_Data.R
@@ -13,7 +13,7 @@ xydb <- curvdb
 for (g in names(xydb)) {
   xydb[[g]] <- merge(xydb[[g]],msdb[[g]][,c('Source', 'N', 'nff', 'Nflig')], all.x=TRUE)
   xydb[[g]] <- merge(xydb[[g]],phend[[g]][,c('Source', 'N', 'HS')], all.x=TRUE)
-  cols <- c('Source', 'N', 'rank','stat','lmax','wmax','A_bl')
+  cols <- c('Source', 'N', 'rank','relative_ranktop','stat','lmax','wmax','A_bl')
   xydb[[g]] <- merge(xydb[[g]],scandim[[g]][,colnames(scandim[[g]])%in%cols], all.x=TRUE)
   for (c in cols)
     if (!c%in%colnames(xydb[[g]]))
@@ -24,9 +24,17 @@ for (g in names(xydb)) {
   dTTflag <- diff(TTflag$TTlig) /  diff(TTflag$nff)
   nffM <- mean(sapply(split(TTem[[g]],TTem[[g]]$N),function(x) x$nff[1]))
   xydb[[g]]$HSest <- slopeM[[g]] * (TT + ifelse(is.na(xydb[[g]]$nff), 0, dTTflag * (xydb[[g]]$nff - nffM)) - TToM[[g]])
+  xydb[[g]]$nffest <- ifelse(is.na(xydb[[g]]$nff),nffM, xydb[[g]]$nff)
+  # ranktop estimation
+  xydb[[g]]$ranktop <-  xydb[[g]]$nffest -  xydb[[g]]$rank + 1
+  # relative_ranktop
+  hs <- ifelse(!is.na(xydb[[g]]$HS), xydb[[g]]$HS, pmin(xydb[[g]]$nffest, xydb[[g]]$HSest))
+  xydb[[g]]$relative_ranktop <- ifelse(!is.na(xydb[[g]]$relative_ranktop), xydb[[g]]$relative_ranktop, hs - xydb[[g]]$rank + 1)
+  # restore stem
+  xydb[[g]][xydb[[g]]$Organ == 0, c('ranktop', 'relative_ranktop')] <- 0
 }
 #
 # Export
 #
-cols <- c('label','Source', 'N', 'var', 'nff', 'Nflig', 'HS', 'HSest','rank','stat','lmax','wmax','A_bl', paste('Pt',1:18,sep=''))
+cols <- c('label','Source', 'N', 'var', 'nff', 'Nflig', 'HS', 'nffest', 'HSest','rank','ranktop', 'relative_ranktop', 'stat','lmax','wmax','A_bl', paste('Pt',1:18,sep=''))
 write.csv(do.call('rbind', sapply(names(xydb), function(g) {dim <- xydb[[g]]; dim$label = g; dim[,cols]}, simplify=FALSE)), 'Compil_curvature.csv',row.names=FALSE)
