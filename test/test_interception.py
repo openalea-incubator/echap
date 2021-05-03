7b73e88227fe95091995e57865c20e5f94a62efc
Marc LABADIE <labadiemarc89@gmail.com>
2021-05-03 15:43:46 +0200

    update import module

--- test/test_interception.py
+++ test/test_interception.py
@@ -1,5 +1,12 @@
-from alinea.echap.imports_echap import *
-
+#from alinea.echap.imports_echap import *
+from alinea.echap.wheat_mtg import *
+from openalea.deploy.shared_data import shared_data
+from alinea.echap.interception_leaf import CaribuInterceptModel
+from alinea.echap.TimeControl import TimeControl
+from alinea.astk.Weather import Weather
+from alinea.popdrops.PesticideInterception import PesticideInterceptionModel
+from alinea.echap.interfaces import pesticide_interception, rain_interception
+import alinea.echap
 
 ############# Pesticide interception
 def test_intercept_caribu():
@@ -25,7 +32,7 @@ def test_intercept_popdrops():
 ############# Rain interception
 def test_intercept_rain():    
     g = adel_mtg()
-    meteo01_filepath = get_shared_data_path(['alinea/echap'], 'meteo01.csv')
+    meteo01_filepath = shared_data(['alinea/echap'])/ 'meteo01.csv'
     t_deb = "2000-10-01 01:00:00"
     weather = Weather(data_file=meteo01_filepath)
     interception_model = RainInterceptionModel()
