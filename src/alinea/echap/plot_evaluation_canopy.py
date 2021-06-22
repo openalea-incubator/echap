2307be4f7c963a0c7efb5631b22e589a3058db6f
Marc LABADIE <labadiemarc89@gmail.com>
2021-06-22 14:15:01 +0200

    fixbug import and if true array

--- src/alinea/echap/plot_evaluation_canopy.py
+++ src/alinea/echap/plot_evaluation_canopy.py
@@ -2,6 +2,7 @@ import numpy
 import scipy.stats
 import matplotlib.pyplot as plt
 
+from alinea.echap.evaluation_leaf_properties import get_simu_results
 
 #from alinea.echap.evaluation_canopy import get_lai_sim, get_light_sim
 
@@ -141,9 +142,9 @@ def plot_sim_obs(df_sim, df_obs=None, variable='LAI_vert', xaxis='HS',
 def test_architecture_canopy_single(variety='Tremie12', nplants=30, nrep=1,
                                     fig_size=(10, 10), color='b', title=None,
                                     axs=None, tight_layout=False):
-    if axs == None:
+    if axs.all() == None:
         fig, axs = plt.subplots(2, 2, figsize=fig_size)
-    df_sim = get_simu_results(variety=variety, nplants=nplants, nrep=nrep)
+    df_sim = get_simu_results(variety=variety, nplants=nplants)
     df_obs = get_all_obs(variety=variety, origin_lai_data='biomass')
     plot_sim_obs(df_sim, df_obs, variable='TCgreen_57', xaxis='HS',
                  xlabel='Haun stage', ylabel='Cover fraction (oblique view)',
